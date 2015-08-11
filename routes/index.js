cc2ca20909ba876762fd2ec8d0f69118a5fcf30e
Stephen Colacurcio <stephencolacurcio@gmail.com>
2015-08-11 12:59:19 -0400

    Got all get routes working

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-//var tweetBank = require('../tweetBank');
+var tweetBank = require('../tweetBank');
 
 
 module.exports = function (io) {
@@ -7,38 +7,44 @@ module.exports = function (io) {
 	router.get('/', function (req, res) {
 		// will trigger res.send of the index.html file
 		// after rendering with swig.renderFile
-		res.render('index', {
-			showForm: true,
-			title: 'Home',
-			tweets: tweetBank.list()
+		tweetBank.list().then(function(tweetList){
+			res.render('index', {
+				showForm: true,
+				title: 'Home',
+				tweets: tweetList
+			});
 		});
 	});
 
 	router.get('/users/:name', function (req, res) {
-		var userTweets = tweetBank.find({
-			name: req.params.name
-		});
-		res.render('index', {
-			showForm: true,
-			title: req.params.name,
-			tweets: userTweets,
-			theName: req.params.name
+
+		tweetBank.findUser(req.params.name)
+		.then(function(tweetList){
+			res.render('index', {
+				showForm: true,
+				title: req.params.name,
+				tweets: tweetList,
+				theName: req.params.name
+			});
 		});
 	});
 
 	router.get('/users/:name/tweets/:id', function (req, res) {
 		var id = parseInt(req.params.id);
-		var theTweet = tweetBank.find({
-			id: id
+		tweetBank.findUser(req.params.name)
+		.then(function(tweetList){
+			res.render('index', {
+				title: req.params.name,
+				tweets: tweetList
+			});
 		});
-		res.render('index', {title: req.params.name, tweets: theTweet})
 	});
 
 	router.post('/submit', function (req, res) {
-		tweetBank.add(req.body.shenanigans, req.body.text);
-		var theNewTweet = tweetBank.list().pop();
-		io.sockets.emit('new_tweet', theNewTweet);
-		res.redirect('/');
+	// 	tweetBank.add(req.body.shenanigans, req.body.text);
+	// 	var theNewTweet = tweetBank.list().pop();
+	// 	io.sockets.emit('new_tweet', theNewTweet);
+	// 	res.redirect('/');
 	});
 	return router;
 };
--- tweetBank.js
+++ tweetBank.js
@@ -1,51 +1,74 @@
 //var _ = require('underscore');
 //var Sequelize = require('sequelize');
-var userTable = require('./models/index').User;
-var tweetTable = require('./models/index').Tweet;
+var User = require('./models/index').User;
+var Tweet = require('./models/index').Tweet;
 
 
-//var data = [];
+var errorCatch = function(error) {
+  console.error(error);
+};
 
 var addUser = function (name, pictureUrl) {
   // data.push({ name: name, text: text, id: data.length });
-  //console.log(userTable.length);
-  userTable.create({id: null, name: name, pictureUrl: pictureUrl});
+  //console.log(User.length);
+User.findOrCreate({where: {name: name}, defaults: {pictureUrl: 'https://s-media-cache-ak0.pinimg.com/236x/7f/75/6b/7f756bcb3481db56650768cc5fc0cf50.jpg'}});
 };
 
-addUser("Chandra", "http://www.fullstackacademy.com/img/team/nimit_maru@2x_nerd.jpg");
+// addUser("Chandra", "http://www.fullstackacademy.com/img/team/nimit_maru@2x_nerd.jpg");
+
+var addTweet = function (name, tweet) {
+   User.findOrCreate({where: {name: name}, defaults: {pictureUrl: 'https://s-media-cache-ak0.pinimg.com/236x/7f/75/6b/7f756bcb3481db56650768cc5fc0cf50.jpg'}
+   }).spread(function(user, created){
+      return user.id;
+  }).then(function(userid) {
+    Tweet.create({id: null, UserId: userid, tweet: tweet});
+  }).catch(errorCatch);
+};
+
+// addTweet('Chandra', 'hi');
+// addTweet('Swigs', 'hey');
+
+var list = function () {
+  //leftJoin Tweet, Users
+  // show username, picture, tweet
+  // User.hasMany(Tweet, {foreignKey: 'UserId'});
+  // Tweet.belongsTo(User, {foreignKey: 'UserId'});
+  return Tweet.findAll({include: [User]})
+  .then(function(t){
+      return t.map(function(elem){
+        var obj = {username: elem.User.name, picture: elem.User.pictureUrl, tweet: elem.tweet};
+        return obj;
+      });
+  }).catch(errorCatch);
+};
+//var t = list();
+// console.log(t);
 
 
-// var list = function () {
-//   return _.clone(data);
-// };
-//
-// var find = function (properties) {
-//   // return _.where(data, properties);
-//
-// };
-//
-// module.exports = { add: add, list: list, find: find };
-//
-// // seeding...
-// var randArrayEl = function(arr) {
-//   return arr[Math.floor(Math.random() * arr.length)];
-// };
-//
-// var getFakeName = function() {
-//   var fakeFirsts = ['Nimit', 'Dave', 'Will', 'Charlotte', 'Jacob','Ethan','Sophia','Emma','Madison'];
-//   var fakeLasts = ["Alley", 'Stacky', 'Fullstackerson', 'Nerd', 'Ashby', 'Gatsby', 'Hazelnut', 'Cookie', 'Tilde', 'Dash'];
-//   return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
-// };
-//
-// var getFakeTweet = function() {
-//   var awesome_adj = ['awesome','breathtaking','amazing','sexy','sweet','cool','wonderful','mindblowing'];
-//   return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The fellows are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
-// };
-//
-// for(var i=0; i<10; i++) {
-//   module.exports.add( getFakeName(), getFakeTweet() );
-// }
 
+var findUser = function (username) {
+    return Tweet.findAll({  include: [{model: User, where: {name: username}}]  })
+    .then(function(t){
+        return t.map(function(elem){
+          var obj = {username: elem.User.name, picture: elem.User.pictureUrl, tweet: elem.tweet};
+          return obj;
+        });
+    });
+};
+
+var findTweet = function (tweetID) {
+    Tweet.findById(tweetID, {include: User})
+    .then(function(t){
+        return {username: t.User.name, picture: t.User.pictureUrl, tweet: t.tweet};
+    });
+};
+
+findTweet(1);
+//console.log(findTweet(1));
 module.exports = {
-  addUser: addUser
+  addUser: addUser,
+  addTweet: addTweet,
+  list: list,
+  findUser: findUser,
+  findTweet: findTweet
 };
